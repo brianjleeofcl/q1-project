12f5de16282098e426497e719a38912049732dfb
Brian Lee <brianjleeofcl@gmail.com>
2016-12-03 19:25:51 -0800

    close #5 Add slide movement for input area

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,14 @@
 (function() {
   'use strict';
 
+  const tabSlideUp = function() {
+    $('#input').slideUp();
+  };
+
   $('#submit-input').on('click', () => {
     event.preventDefault();
 
-  })
+    tabSlideUp();
+  });
 
 })();
