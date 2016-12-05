6ac263da7a54b6a4cfdff2b81ffe5eb6b730655c
Brian Lee <brianjleeofcl@gmail.com>
2016-12-04 16:01:57 -0800

    Lint

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,15 +5,14 @@
     event.preventDefault();
     const $target = $('.current');
 
-    $target.toggleClass('current grey-text')
-    $target.find('input').prop('disabled', true)
-    $target.next().toggleClass('current hide')
-    $(event.target).prop('disabled', true)
-  })
-
-  $('#submit-input').on('click', () => {
-    $("#input").css('min-height', 0)
-    $('#input').slideUp();
+    $target.toggleClass('current grey-text');
+    $target.find('input').prop('disabled', true);
+    $target.next().toggleClass('current hide');
+    $(event.target).prop('disabled', true);
   });
 
+  $('#submit-input').on('click', () => {
+    $('#input').css('min-height', 0);
+    $('#input').slideUp();
+  });
 })();
