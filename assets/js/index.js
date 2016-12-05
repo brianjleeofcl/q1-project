0ffbc6b274f2934ad3df772a6340231154233de0
Brian Lee <brianjleeofcl@gmail.com>
2016-12-04 18:41:09 -0800

    #10 Add questions 1-3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,22 @@
 (function() {
   'use strict';
 
-  $('.question-field').on('click', '.current button', () => {
-    event.preventDefault();
-    const $target = $('.current');
+  const inputValidation = function() {
+    return
+  };
 
+  const displayNextQ = function($target) {
     $target.toggleClass('current grey-text');
     $target.find('input').prop('disabled', true);
+    $target.find('button').prop('disabled', true);
     $target.next().toggleClass('current hide');
-    $(event.target).prop('disabled', true);
+  }
+
+  $('.question-field').on('click', '.current button', () => {
+    event.preventDefault();
+    const $curQuestion = $('.current');
+
+    displayNextQ($curQuestion);
   });
 
   $('#submit-input').on('click', () => {
