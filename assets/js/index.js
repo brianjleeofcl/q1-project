d692d8f295c7df6627d410d1e73ff56e9a0ac17b
Brian Lee <brianjleeofcl@gmail.com>
2016-12-05 12:02:37 -0800

    #8 Add controls on numbers

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,24 @@
   $('select').material_select();
   $('.modal').modal();
 
-  const inputValidation = function() {
-    return
+  const inputValidation = function($target) {
+    const targetType = $target.find('input').attr('type');
+
+    console.log(targetType);
+    if ($target.find('input').val() && targetType === 'number') {
+      return true;
+    }
+    else if (targetType === 'number') {
+      return false;
+    }
+
+    const radioVal = [];
+
+    $('input:radio:enabled:checked').each((index, element) => {
+      radioVal.push($(element).val());
+    })
+
+    console.log(radioVal);
   };
 
   const displayNextQ = function($target) {
@@ -15,11 +31,23 @@
     $target.next().toggleClass('current hide');
   }
 
+  const collectData = function() {
+    const obj = {};
+    $('.input-field').find('input').each((index, element) => {
+      obj[element.id] = element.value;
+    })
+    return obj
+  }
+
   $('.question-field').on('click', '.current button', () => {
     event.preventDefault();
     const $curQuestion = $('.current');
 
-    displayNextQ($curQuestion);
+    if (inputValidation($curQuestion)) {
+      displayNextQ($curQuestion);
+    } else {
+      Materialize.toast('Please enter a valid response.', 2000)
+    }
   });
 
   $('select.rules').on('change', () => {
@@ -30,15 +58,18 @@
     } else {
       $('.suits input[type="radio"]').prop('disabled', true);
     }
+
     if (selected.includes('value')) {
       $('.values input[type="radio"]').prop('disabled', false);
     } else {
       $('.values input[type="radio"]').prop('disabled', true);
     }
-  })
+  });
 
   $('#submit-input').on('click', () => {
     $('#input').css('min-height', 0);
     $('#input').slideUp();
+    const data = collectData();
+    console.log(data);
   });
 })();
