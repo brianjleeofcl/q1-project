3b7190a98fc9cf0573b210f462637421a7d3062d
Brian Lee <brianjleeofcl@gmail.com>
2016-12-06 09:47:51 -0800

    close #17 Add calculation displays

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,8 +9,8 @@
   let maxRun = 0;
   let currentState = {
     occurrence: 0,
-    total:0
-  }
+    total: 0
+  };
 
   const numberValidate = function($input) {
     if ($input.val() !== '') {
@@ -18,7 +18,7 @@
     } else {
       return false;
     }
-  }
+  };
 
   const inputValidation = function($target) {
     const targetType = $target.find('input').attr('type');
@@ -28,11 +28,11 @@
     }
 
     const radioVal = [];
-    const radioCat = $('select.rules').val()
+    const radioCat = $('select.rules').val();
 
     $('input:radio:enabled:checked').each((index, element) => {
       radioVal.push($(element).val());
-    })
+    });
 
     if (radioCat.length !== 0 && radioVal.length === radioCat.length) {
       return true;
@@ -50,25 +50,26 @@
 
   const collectData = function() {
     inputData = {};
+    inputData.rule = {};
 
     $('.input-field').find('input[type="number"]').each((index, element) => {
       inputData[element.id] = element.value;
-    })
+    });
     $('.input-field').find('input:radio:checked').each((index, element) => {
-      inputData[element.name] = element.value;
-    })
+      inputData.rule[element.name] = element.value;
+    });
 
-    maxRun = parseInt(inputData.q4)
-    console.log(maxRun);
+    maxRun = parseInt(inputData.q4);
+    console.log(inputData);
   };
 
   const drawDeck = function(id, deckCount) {
-    const cardCount = deckCount * 52
+    const cardCount = deckCount * 52;
     const $xhr = $.ajax({
       method: 'GET',
-      url:`https://deckofcardsapi.com/api/deck/${id}/draw/?count=${cardCount}`,
+      url: `https://deckofcardsapi.com/api/deck/${id}/draw/?count=${cardCount}`,
       dataType: 'json'
-    })
+    });
 
     $xhr.done((result) => {
       if ($xhr.status !== 200) {
@@ -76,21 +77,25 @@
       }
 
       deck = result.cards.map((obj) => {
+        console.log(obj);
         const card = {};
+
         card.code = obj.code;
         card.images = obj.images;
-        if (obj.value === inputData.value && obj.suit === inputData.suit) {
+
+        if (obj.value === inputData.rule.value && obj.suit === inputData.rule.suit) {
           card.condition = true;
-        } else if (typeof(inputData.suit) === 'undefined' && obj.value === inputData.value) {
+        } else if (typeof inputData.suit === 'undefined' && obj.value === inputData.value) {
           card.condition = true;
-        } else if (typeof(inputData.value) === 'undefined' && obj.suit === inputData.suit) {
+        } else if (typeof inputData.value === 'undefined' && obj.suit === inputData.suit) {
           card.condition = true;
         } else {
-          card.condition = false
+          card.condition = false;
         }
+
         return card;
-      })
-    })
+      });
+    });
   };
 
   const generateDeck = function(data) {
@@ -109,7 +114,22 @@
 
       drawDeck(deckID, data.q1);
     });
-  }
+  };
+
+  const displayCalculation = function(obj) {
+    const arr = Object.keys(obj);
+
+    if (arr.length === 2) {
+      $('#calculation').text('1/4 x 1/13');
+      $('#calc-prob').text('1/52 = 1.92%');
+    } else if (arr.length === 1 && arr.includes('suit')) {
+      $('#calculation').text('1/4');
+      $('#calc-prob').text('1/4 = 25.00%');
+    } else if (arr.length === 1 && arr.includes('value')) {
+      $('#calculation').text('1/13');
+      $('#calc-prob').text('1/13 = 7.69%');
+    }
+  };
 
   $('.question-field').on('click', '.current button', () => {
     event.preventDefault();
@@ -118,7 +138,7 @@
     if (inputValidation($curQuestion)) {
       displayNextQ($curQuestion);
     } else {
-      Materialize.toast('Please enter a valid response.', 2000)
+      Materialize.toast('Please enter a valid response.', 2000);
     }
   });
 
@@ -143,15 +163,16 @@
     $('#input').slideUp();
     collectData();
     generateDeck(inputData);
+    displayCalculation(inputData.rule);
   });
 
   const drawCard = function() {
     const len = deck.length;
-    return deck[Math.floor(Math.random() * len)]
+    return deck[Math.floor(Math.random() * len)];
   };
 
   const updateMeasurement = function(obj) {
-    if (obj.condition){
+    if (obj.condition) {
       currentState.occurrence++;
     }
     currentState.total++;
@@ -161,34 +182,39 @@
     const card = drawCard();
 
     $('#card-image img').attr('src', card.images.png);
+    if (card.condition) {
+      $('#card-image img').addClass('occurrence');
+    } else {
+      $('#card-image img').removeClass('occurrence');
+    }
 
     updateMeasurement(card);
     $('.mes-oc').text(currentState.occurrence);
     $('.mes-to').text(currentState.total);
-    $('.mes-pr').text((currentState.occurrence / currentState.total * 100).toFixed(2))
+    $('.mes-pr').text((currentState.occurrence / currentState.total * 100).toFixed(2));
   };
 
   const execution = function(fn, duration){
     let intervalID;
 
     return {
-      start : function() {
+      start() {
         intervalID = setInterval(fn, duration);
       },
-      stop : function() {
+      stop() {
         clearInterval(intervalID);
       }
-    }
-  }
+    };
+  };
 
   const timer = execution(() => {
-    renderCard()
-    maxRun --;
+    renderCard();
+    maxRun--;
   }, 1000);
 
   $('button[name="start"]').on('click', () => {
-    $('button[name="start"]').toggleClass('hide')
-    $('button[name="pause"]').toggleClass('hide')
+    $('button[name="start"]').toggleClass('hide');
+    $('button[name="pause"]').toggleClass('hide');
     timer.start();
   });
 
